49b03d98fadfa9c0614088194483a6ac1674b789
Sanidhya Mahajan <sanidhya10628@gmail.com>
2021-05-22 10:11:49 +0530

    style added

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Current Equation for solving
 let str='';
-console.log(str)
+//console.log(str)
 
 // Click event for Numbers Click
 let numbers = document.getElementsByClassName('number')
@@ -13,7 +13,7 @@ for (let i = 0;i<numbers.length;i++)
     
     str = str + a;
     console.log(str);
-    document.getElementById('equation').innerHTML = str;
+    document.getElementById('show').innerHTML = str;
   })
 }
 
@@ -26,25 +26,39 @@ for(let i = 0;i<operators.length;i++)
     let a = document.getElementsByClassName('operator')[i].innerHTML;
     console.log(a);
 
-    if(str===''){
-      alert("Invalid Syntax!");
-    }
-    else{
-      str = str + a;
-      document.getElementById('equation').innerHTML = str;
-    }
+    // if(str===''){
+    //   //alert("Invalid Syntax!");
+    // }
+    // else{
+    //   str = str + a;
+    //   document.getElementById('show').innerHTML = str;
+    // }
+    str = str + a;
+      document.getElementById('show').innerHTML = str;
   })
 }
 
 // Click Event for Clear
 document.querySelector('#clear').addEventListener('click',function(){
   str = '';
-  document.getElementById('equation').innerHTML = '';
+  //document.getElementById('equation').innerHTML = '';
+  document.getElementById('show').innerHTML = '';
 })
 
 // Click Event for Equals Operator
 document.querySelector('#equals').addEventListener('click',function(){
   // let num1 = Number(str[0]);
   // let num2 = Number(str[2]);
-  document.getElementById('show').innerHTML = eval(str);
+  if(str==='')
+  {
+
+  }
+  else
+  {
+    document.getElementById('show').innerHTML = eval(str);
+    //document.getElementById('equation').innerHTML = '';
+    //str = '';
+    str = eval(str);
+  }
 })
+
